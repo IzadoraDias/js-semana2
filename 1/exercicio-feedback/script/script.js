37ef243828d27e866d92818e602f7355cc304c67
IzadoraDias <izzadoradias@gmail.com>
2018-09-17 16:51:27 -0300

    exercícios JS semana 2

---
+++ 1/exercicio-feedback/script/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".feedback__button");
+
+button.addEventListener("click", function(event){
+    event.preventDefault(); //previne o evento padrão
+alert("Izagatinha")
+    //const inputMessage =  document.getElementById("feedbackInputMessage");
+
+})
