9390f0e27b83730da2089b9b846998f9161f256a
IzadoraDias <izzadoradias@gmail.com>
2018-09-21 16:09:19 -0300

    correção exercício twitter

--- 4/exemplo-1-twitter/script/script.js
+++ 4/exemplo-1-twitter/script/script.js
@@ -1,50 +1,40 @@
-//pega o botão twittar
-const button = document.querySelector(".tweet-composer__button");
+const inputTweet = document.getElementById("tweetComposerInput")
+const buttonCreateTweet = document.querySelector(".tweet-composer__button")
 
-//pega o texto e coloca na na linha
-const inputTweet = document.getElementById("tweetComposerInput");
-const tweetsTimeline = document.querySelector(".tweets-timeline");
+const tweetsTimeline = document.querySelector(".tweets-timeline")
 
-//ação do botão
- button.addEventListener ("click", function(event){
-   event.preventDefault ();
+buttonCreateTweet.addEventListener("click", function(e){
+    e.preventDefault();
+    
+    const boxTweet =  document.createElement("div");
 
-//cria a div que abraça todo o conteudo
- const div = document.createElement("div");
- div.className = "tweets-timeline__box"
+    //criei um elemento novo pro tweet
+    const newTweetP = document.createElement("p");
+    //passei o conteúdo do tweet pro paragrafo
+    newTweetP.innerHTML = inputTweet.value;
 
-//cria o header
-const header = document.createElement("div");
-header.className = "tweets-timeline__box"
-header.innerHTML = `<span class="tweets-timeline__name">Iza</span>
-<span class="tweets-timeline__username">@Izacomz</span>
-<span class="tweets-timeline__date">${new Date}</span>`
+    //criei header pro box tweet    
+    const header = document.createElement("div");
+    header.innerHTML = `
+    <div class="tweets-timeline__header">
+                    <span class="tweets-timeline__name">ana.js</span>
+                    <span class="tweets-timeline__username">@naluhh</span>
+                    <span class="tweets-timeline__date">1 de ago</span>
+                </div>
+    `
 
-//cria elemento novo pro tweet
-   const newTweetP= document.createElement("p");
-//passei o conteúdo do tweet pro paragrafo
-   newTweetP.innerHTML = inputTweet.value;
+    //criei o footer pro box do tweet
+    const footer = document.createElement("div");
+    footer.innerHTML = `<div class="tweets-timeline__footer">
+    tweet fixado
+</div>`
+    
 
-//footer
-const footer = document.createElement("div");
-footer.className = "tweets-timeline__footer"
-footer.innerHTML = `<button class="tweets-timeline__delete-button button"> Excluir </button>`
-
-//coloca o header + texto do tweet na div
-div.appendChild(header);
-div.appendChild(newTweetP);
-div.appendChild(footer);
-
-//coloca a div na timeline
- tweetsTimeline.insertBefore(div, tweetsTimeline.childNodes[0]);
- inputTweet.value = "";
-
-//deletar twitter
-const buttonDelete = document.querySelector(".tweets-timeline__delete-button");
-
-  buttonDelete.addEventListener('click', function(evento){
-  evento.preventDefault();
-  div.remove();
-
+    boxTweet.appendChild(header);
+    boxTweet.appendChild(newTweetP);
+    boxTweet.appendChild(footer);
+    
+    //coloquei a div na timeline
+    tweetsTimeline.insertBefore(boxTweet, tweetsTimeline.childNodes[0]);
 })
-})
+
---
+++ 5/exemplo-menu/script/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".sidebar-menu__button");
+const sidebar = document.querySelector(".sidebar-menu");
+
+button.addEventListener("click", function(event){
+
+    
+}
